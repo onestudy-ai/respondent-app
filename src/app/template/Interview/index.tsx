5de62102ba7d55d1e9be12315810529aa9a0463e
Rick Blalock <rblalock@gmail.com>
2024-02-17 17:18:42 -0500

    Fix issue wiuth video overlay not shwoing.

--- src/app/template/Interview/index.tsx
+++ src/app/template/Interview/index.tsx
@@ -16,7 +16,7 @@ export const InterviewWrapper = (props: InterviewProps) => {
 	const [watchedEmbed, setWatchedEmbed] = useState(false);
 
 	return (
-		<div className="relative isolate overflow-hidden dark:bg-stone-950 min-h-screen flex-col items-center justify-between">
+		<div className="flex relative isolate overflow-hidden dark:bg-stone-950 min-h-screen flex-col items-center justify-between">
 			{/* Header */}
 			<div className="w-full flex justify-between items-center px-3 py-5">
 				<div className="flex items-center">
@@ -42,12 +42,14 @@ export const InterviewWrapper = (props: InterviewProps) => {
 				<div className="w-full flex flex-col justify-center items-center px-3 py-5">
 					<EmbedWrapper url={props.interview?.study?.meta?.embedUrls[0]} />
 
-					<Button className="mt-5" size="lg" onClick={() => setWatchedEmbed(true)}>
-						Continue
-					</Button>
+					<div className="w-full h-20 flex justify-center">
+						<Button className="mt-5" size="lg" onClick={() => setWatchedEmbed(true)}>
+							Continue
+						</Button>
+					</div>
 				</div>
 			) : (
-				<div className="mx-auto px-2 pb-24 pt-5 lg:pt-20 sm:pb-32 lg:flex lg:px-8">
+				<div className="w-full mx-auto px-2 pb-24 pt-5 lg:pt-20 sm:pb-32 lg:flex lg:px-8">
 					<div className="flex w-full flex-1 flex-col items-center">
 						<ConversationWrapper interview={props.interview} />
 					</div>
@@ -55,8 +57,8 @@ export const InterviewWrapper = (props: InterviewProps) => {
 			)}
 
 			{/* Footer */}
-			<div className="absolute inset-x-0 bottom-0">
-				<div className="flex items-center justify-between m-5 text-xs text-gray-400">
+			<div className="w-full">
+				<div className="flex w-full text-left m-5 text-xs text-gray-400">
 					<a href="https://onestudy.ai">Powered by <strong>OneStudy</strong></a>
 				</div>
 			</div>
